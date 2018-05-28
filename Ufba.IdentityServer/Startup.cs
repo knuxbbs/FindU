d997bdb4205ed00ff0f68c1997df93e51f2b8c46
knuxbbs <knuxbbs@gmail.com>
2018-05-27 23:11:49 -0300

    Adiciona IdentityServer

---
+++ Ufba.IdentityServer/Startup.cs
@@ -0,0 +1,59 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using System.Linq;
+
+namespace Ufba.IdentityServer
+{
+	public class Startup
+	{
+		// This method gets called by the runtime. Use this method to add services to the container.
+		// For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
+		public void ConfigureServices(IServiceCollection services)
+		{
+			services.AddMvc();
+
+			services.AddIdentityServer()
+					.AddTemporarySigningCredential()
+					.AddInMemoryClients(IdentityServerConfiguration.GetClientScope())
+					.AddInMemoryApiResources(IdentityServerConfiguration.GetApiResources())
+					.AddInMemoryIdentityResources(IdentityServerConfiguration.GetIdentityResources())
+					.AddTestUsers(IdentityServerConfiguration.GetUsers().ToList());
+		}
+
+		// This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+		public void Configure(IApplicationBuilder app, IHostingEnvironment env, ILoggerFactory loggerFactory)
+		{
+			loggerFactory.AddConsole(Configuration.GetSection("Logging"));
+			loggerFactory.AddDebug();
+
+			if (env.IsDevelopment())
+			{
+				app.UseDeveloperExceptionPage();
+				app.UseBrowserLink();
+			}
+			else
+			{
+				app.UseExceptionHandler("/Home/Error");
+			}
+
+			app.UseIdentityServer();
+
+			app.UseStaticFiles();
+
+			app.UseMvc(routes =>
+			{
+				routes.MapRoute(
+					name: "default",
+					template: "{controller=Home}/{action=Index}/{id?}");
+			});
+
+			app.Run(async (context) =>
+			{
+				await context.Response.WriteAsync("Hello World!");
+			});
+		}
+	}
+}
