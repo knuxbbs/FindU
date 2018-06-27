baf9083b6b53f7c498f5ca8463c5eceb07425263
alessonbruh <alessonbruh@hotmail.com>
2018-06-27 19:58:14 -0300

    tela de cadastro

---
+++ Projeto_FindU/Projeto_FindU/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Projeto_FindU.Startup))]
+namespace Projeto_FindU
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
